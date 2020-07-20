a677bc1feb389a162e68fcc703f38cfdfc699330
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-07-20 14:25:27 -0300

    fix laudo migration

--- db/migrate/20200718103851_create_laudos.rb
+++ db/migrate/20200718103851_create_laudos.rb
@@ -6,15 +6,15 @@ class CreateLaudos < ActiveRecord::Migration[5.1]
         # Referencia / Tabela Propriedades
         # t.string :proprietario # Nome do Proprietario @propriedade.proprietario
         # t.references :propriedade # Referencia da tabela propriedades
-        
+        t.references :insumo, foreing_key: true
         # Referencia / Tabela  Resultados Quimicos
-        t.references :resultados_quimicos
+        t.references :resultados_quimicos, foreing_key: true
 
         # Referencia  / Tabela Indicadores_saturacao
-        t.references :indicadores_saturacao
+        t.references :indicadores_saturacao, foreing_key: true 
 
         # Referencia  / Tabela Disponibilidade no Solo
-        t.references :disponibilidade_solo
+        t.references :disponibilidade_solo, foreing_key: true
 
         t.timestamps
         end
