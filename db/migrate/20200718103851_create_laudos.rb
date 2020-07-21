5f2a94c674cae13fc0b7dd2490fe2601165247c4
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-07-21 15:12:48 -0300

    corrigindo migrações

--- db/migrate/20200718103851_create_laudos.rb
+++ db/migrate/20200718103851_create_laudos.rb
@@ -3,17 +3,17 @@ class CreateLaudos < ActiveRecord::Migration[5.1]
         create_table :laudos do |t|
         # Referencias do usuario atual
         t.string :solicitante # Nome do Tecnico @current_user.first_name && last_name
-        t.references :user_id, foreign_key: true # Referencia do usuario @current_user.ids.first
+        t.references :user, foreign_key: true # Referencia do usuario @current_user.ids.first
     
         # Referencia / Tabela Propriedades
         t.references :property, foreign_key: true
-        t.references :proprietario, foreign_key: true
+        t.string :proprietario
         
         # t.string :proprietario # Nome do Proprietario @propriedade.proprietario
         # t.references :propriedade # Referencia da tabela propriedades
         
         # Referencia / Tabela de insumos
-        t.references :insumo_id, foreign_key: true
+        t.references :insumo, foreign_key: true
         
         # Referencia / Tabela  Resultados Quimicos
         # t.references :resultados_quimicos, foreign_key: true
--- db/migrate/20200718103852_add_laudos_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddLaudosToUsers < ActiveRecord::Migration[5.1]
-    def change
-      add_column :users, :laudo_id, :references, :foreign_key true
-    end
-  end
-  
