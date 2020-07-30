9c7f4364b39bf8caf9e9a21765c4479b7903c0dd
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-07-30 12:22:49 -0300

    remove minified js and add original one

---
+++ app/assets/javascripts/grid-tui.js
@@ -0,0 +1,35 @@
+const grid = tui.Grid;
+
+const instance = new Grid({
+    el: document.getElementById('grid'), // Container element
+    columns: [
+      {
+        header: 'Name',
+        name: 'name'
+      },
+      {
+        header: 'Artist',
+        name: 'artist'
+      },
+      {
+        header: 'Release',
+        name: 'release'
+      },
+      {
+        header: 'Genre',
+        name: 'genre'
+      }
+    ],
+    data: [
+      {
+        name: 'Beautiful Lies',
+        artist: 'Birdy',
+        release: '2016.03.26',
+        genre: 'Pop'
+      }
+    ]
+  });
+  
+//   instance.resetData(newData); // Call API of instance's public method
+  
+grid.applyTheme('striped'); // Call API of static method
