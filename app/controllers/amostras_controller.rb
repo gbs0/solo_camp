707129ba907db169e5233e9c47bc678915074fec
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-10-14 09:32:05 -0300

    finish controller methods

--- app/controllers/amostras_controller.rb
+++ app/controllers/amostras_controller.rb
@@ -15,9 +15,9 @@ class AmostrasController
 		@amostra = Amostra.new(amostra_params)
 		p @amostray.user_id = @user.id
 		# @property.ownership => Igual ao ownership assimilado no params.require( :name, :last_name)
-		if @property.save
-		  flash[ :notice ] = "'#{@property.name}' salvo."
-		  redirect_to properties_path, notice: "A nova propriedade foi adicionado"
+		if @amostra.save
+		  flash[ :notice ] = "'#{@amostra}' salvo."
+		  redirect_to properties_path, notice: "A nova amostra foi adicionada"
 		else
 		  flash[:alert] = "Erro, verifque os campos digitados"
 		  render :new
@@ -25,21 +25,22 @@ class AmostrasController
 	end
 	
 	def update
-	  @property.update(property_params)
+	  @amostra.update(amostra_params)
 	end
 
 	private
 	
-	def property_params
-	  params.require(:property).permit( :name, :address, :city, :uf, :cep, :area )
+	def amostra_params
+	  params.require(:amostra).permit( :name, :address, :city, :uf, :cep, :area )
 	end
 
 	def set_user
 	  @user = current_user
-	end 
+    end 
+    
 	def get_name_params
-	end
-
+    end]
+    
 	def set_ownerships
 	  @ownerships = Ownership.where(user: @user)
 	end
