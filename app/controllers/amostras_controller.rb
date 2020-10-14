9ea2f475f1e64dc705d5d2cb9227396d5ed48a9f
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-10-14 09:29:51 -0300

    add methods for controller amostras

--- app/controllers/amostras_controller.rb
+++ app/controllers/amostras_controller.rb
@@ -1,4 +1,46 @@
 class AmostrasController
-  def index
-  end
+    def index
+		# Listar propriedades do current_user
+		@amostras = Amostra.where(user_id: @user.id)
+	end
+	
+	def new
+	  respond_to do |format|
+		@amostra = Amostra.new
+		format.js
+	  end	
+	end
+
+	def create
+		@amostra = Amostra.new(amostra_params)
+		p @amostray.user_id = @user.id
+		# @property.ownership => Igual ao ownership assimilado no params.require( :name, :last_name)
+		if @property.save
+		  flash[ :notice ] = "'#{@property.name}' salvo."
+		  redirect_to properties_path, notice: "A nova propriedade foi adicionado"
+		else
+		  flash[:alert] = "Erro, verifque os campos digitados"
+		  render :new
+		end
+	end
+	
+	def update
+	  @property.update(property_params)
+	end
+
+	private
+	
+	def property_params
+	  params.require(:property).permit( :name, :address, :city, :uf, :cep, :area )
+	end
+
+	def set_user
+	  @user = current_user
+	end 
+	def get_name_params
+	end
+
+	def set_ownerships
+	  @ownerships = Ownership.where(user: @user)
+	end
 end
