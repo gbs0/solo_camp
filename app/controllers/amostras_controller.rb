17386cbf1434120ad7faaee84036fe8463b19179
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-10-19 10:23:43 -0300

    add filter on get_ownerships fix

--- app/controllers/amostras_controller.rb
+++ app/controllers/amostras_controller.rb
@@ -1,7 +1,7 @@
 class AmostrasController < ApplicationController
     before_action :set_user
 	before_action :get_ownerships, only: [:new, :create]
-	
+
     def index
 		# Listar propriedades do current_user
 		# @amostras = Amostra.where(user_id: @user.id)
@@ -49,7 +49,7 @@ class AmostrasController < ApplicationController
 	end
 	
 	def get_ownerships
-	  @ownerships = Ownership.all(user_id: user.id)
+	  @ownerships = Ownership.where(user_id: @user.id)
 	end
 
 end
