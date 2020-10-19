cadebbf0be79a2b16bed033e755f62bb3f74d442
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-10-19 11:56:38 -0300

    add error in new view for amostra creation

--- app/models/amostra.rb
+++ app/models/amostra.rb
@@ -1,2 +1,27 @@
 class Amostra < ApplicationRecord
+  belongs_to :user, foreign_key: :user_id
+    # belongs_to :laudo, foreign_key: :property
+  
+  validate :owner_name,
+    :property_name,
+    :profundidade,
+    :compactacao,
+    :peso,
+    :user_id,
+    :argila,
+    :potassio,
+    :calcario,
+    :magnesio,
+    :enxofre20,
+    :enxofre40,
+    :hidrogenio,
+    :alcalinidade,
+    :boro,
+    :cobre,
+    :manganes,
+    :zinco,
+    :carbono,
+    :materia,
+    :valor,
+    :ctc
 end
