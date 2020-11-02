4cd1934bdbf15101f99c9be3c790f9c10eace272
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-11-02 16:11:40 -0300

    add refactored controllers and actions

---
+++ app/models/analise.rb
@@ -0,0 +1,7 @@
+class Analise < ApplicationRecord
+    belongs_to :user, foreign_key: :user
+    has_one :property
+    has_one :ownership, through: :property, foreign_key: :proprietario
+    has_one :insumo
+    has_many :amostras, foreign_key: :amostra
+  end
