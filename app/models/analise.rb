323b8d51de6830d3abc1e55d6a31e71988d40601
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-11-13 12:01:56 -0300

    refactoring some models

--- app/models/analise.rb
+++ app/models/analise.rb
@@ -1,7 +1,8 @@
 class Analise < ApplicationRecord
     belongs_to :user, foreign_key: :user
     has_one :property
-    has_one :ownership, through: :property, foreign_key: :proprietario
+    # has_one :ownership, through: :property, foreign_key: :proprietario
+    has_one :ownership, foreign_key: :ownership_id
     has_one :insumo
     has_many :amostras, foreign_key: :amostra
   end
