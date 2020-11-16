3f794e2acb1bf25da19b5e15b21d945077954131
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-11-16 10:47:27 -0300

    implementing show action for amostras

--- app/controllers/amostras_controller.rb
+++ app/controllers/amostras_controller.rb
@@ -14,6 +14,10 @@ class AmostrasController < ApplicationController
 	  end	
 	end
 
+	def show
+	  @amostra = Amostra.find(params[:id])
+	end
+
 	def create
 		@amostra = Amostra.new(amostra_params)
 		@amostra.user_id = @user.id
