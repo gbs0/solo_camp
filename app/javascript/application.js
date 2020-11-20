907458ecc8185a283980f33bee2812d268d05153
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-11-20 10:13:01 -0300

    add upgrade for rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,22 @@
-console.log("Hello from application.js");
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("channels")
+
+import 'select2/dist/css/select2.css';
+
+import { multipleSelect } from '../components/select2';
+
+console.log("Olá de application.js")
+
+multipleSelect();
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
