1b6cb41ef6cb5b0e0443199e0ff9732a4ff9f2d0
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-12-03 18:36:46 -0300

    add rspec test init

--- test/models/ownership_test.rb
+++ test/models/ownership_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class OwnershipTest < ActiveSupport::TestCase
+  test "Should Initialize new Ownership instance" do
+    ownership = Ownership.new
+
+    assert_not ownership.save, "Saved the article without a title"
+  end
   # test "the truth" do
   #   assert true
   # end
