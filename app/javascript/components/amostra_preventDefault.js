1e637060eea0ef2fcb4bc4d10c5a992489a17084
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-01-12 15:19:18 -0300

    add some front end updates

---
+++ app/javascript/components/amostra_preventDefault.js
@@ -0,0 +1,12 @@
+// const stepDownBtn = document.getElementsByClassName("minus")[0];
+// const stepUpBtn = document.getElementsByClassName("plus")[0];
+
+// stepDownBtn.addEventListener("click", (e) => {
+//     console.log(e);
+//     e.preventDefault();
+// })
+
+// stepUpBtn.addEventListener("click", (e) => {
+//     console.log(e);
+//     e.preventDefault();
+// })
