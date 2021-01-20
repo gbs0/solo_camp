95c30fb89df4c28a3e3f6dbad62880da2f0c658a
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-01-20 10:07:26 -0300

    add pure js counter

--- app/javascript/components/amostra_counter.js
+++ app/javascript/components/amostra_counter.js
@@ -1,5 +1,15 @@
 const amostraCounterBtn = {
-
+    $('#qty_input').prop('disabled', true);
+    
+    $('#plus-btn').click(function(){
+    	$('#qty_input').val(parseInt($('#qty_input').val()) + 1 );
+    });
+    $('#minus-btn').click(function(){
+    	$('#qty_input').val(parseInt($('#qty_input').val()) - 1 );
+    	if ($('#qty_input').val() == 0) {
+			$('#qty_input').val(1);
+		}
+    });
 };
 
 export { amostraCounterBtn };
