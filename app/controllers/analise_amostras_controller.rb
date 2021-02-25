8221da7db5a2b800bc11886eaa8fb586b4435167
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-02-25 17:47:05 -0300

    Init some actions

--- app/controllers/analise_amostras_controller.rb
+++ app/controllers/analise_amostras_controller.rb
@@ -1,15 +1,17 @@
 class AnaliseAmostrasController < ApplicationController
-  before_action :set_user, only: [:new, :create]
+  before_action :set_user, only: [:new, :create, :edit, :update]
 
+  def index; end
+
+  def edit; end
+  
   def new
     @amostras = Amostra.where(user_id: :set_user.id)
     # pegar as amostras daquele user com a propriedade vinda do backend
     @analise_amostra = AnaliseAmostra.new
   end
 
-  def
-
-  # def create
+  # def create # Criar uma análise utilizando Background Job
   #   @amostras = Amostra.where(params[:amostra_id])
   #   @amostras.each do |amostra|
   #     analise_amostra = AnaliseAmostra.new
@@ -20,6 +22,21 @@ class AnaliseAmostrasController < ApplicationController
   #   end
   # end
 
+  def update
+    @analise_amostra = AnaliseAmostra.find(params[:id])
+
+    rescue => e
+      @error = e.message
+    ensure
+      respond_to do |format|
+        format.html { redirect_to analise_amostras_path, flash: {sucess: "Analise de Solo Editada com sucesso!"}}
+      end
+  end
+
+  def destroy
+    @analise_amostra = AnaliseAmostra.find(params[:id])
+
+  end
   private
 
   def analise_amostra_params
