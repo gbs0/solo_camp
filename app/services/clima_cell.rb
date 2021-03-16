d8b495f80dd05063c398f8e24321d3601714e787
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-03-16 10:41:52 -0300

    add code service

--- app/services/clima_cell.rb
+++ app/services/clima_cell.rb
@@ -4,9 +4,15 @@ class ClimaCell < ApplicationService
     def initialize(_lat, _lng)
         @lat = _lat
         @lng = _lng
+        @key = Rails.application.secrets['clima_key'] if Rails.env.development?
     end
 
 
-    def call; end
+    def call 
+        fetch = `curl --request GET --url \
+        'https://data.climacell.co/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=temperature&timesteps=1h&units=metric&apikey=#{@key}'`
+        response = JSON.parse(fetch)
+        binding.pry
+    end
     
 end
