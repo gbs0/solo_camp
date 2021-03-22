fdb72443f72e1c4b9dbc6be5d2468b1b24c84d22
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-03-22 15:46:57 -0300

    return on method

--- app/services/maps.rb
+++ app/services/maps.rb
@@ -9,7 +9,7 @@ class Maps < ApplicationService
 
     def call
         unless @key.nil?
-            place_url = "https://maps.googleapis.com/maps/api/staticmap?center=#{@lat},#{@lng}&zoom=15&size=800x500&maptype=roadmap
+            "https://maps.googleapis.com/maps/api/staticmap?center=#{@lat},#{@lng}&zoom=15&size=800x500&maptype=roadmap
             &markers=color:green%7Clabel:S%7C#{@lat},#{@lng}&key=#{@key}"
         end
     end
