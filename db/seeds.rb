96493211949bd0149f979853aeb17274206ed863
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-04-02 15:27:37 -0300

    refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -141,16 +141,13 @@ case Rails.env
       puts "✅ Done Creating Analises..."
       
       puts "Creating AnaliseAmostras"
-      # amostra_one = Amostra.first
-      # amostra_json_one = Amostra.serialize(amostra_one)
-      
       amostra_two = Amostra.last
       amostra_json_two = Amostra.serialize(amostra_two)
 
       @analise_amostras = AnaliseAmostra.find_or_create_by(
                         user_id: User.first.id,
                         analise_id: Analise.first.id,
-                        amostras: amostra_json_two,
+                        amostra: amostra_json_two,
                         insumo: Analise.first.insumo_name
                       )
       @analise_amostras.save!
